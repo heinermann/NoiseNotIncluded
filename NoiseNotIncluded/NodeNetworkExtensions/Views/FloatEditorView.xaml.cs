9d85c22c92ebe53efb8b45b45576baa6fecb1a54
Heinermann <aheinerm@gmail.com>
2020-09-27 12:26:42 -0700

    Fixed float editor

--- NoiseNotIncluded/NodeNetworkExtensions/Views/FloatEditorView.xaml.cs
+++ NoiseNotIncluded/NodeNetworkExtensions/Views/FloatEditorView.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reactive.Disposables;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NodeNetworkExtensions.ViewModels;
+using ReactiveUI;
 
-namespace NoiseNotIncluded.NodeNetwork.Views
+namespace NodeNetworkExtensions.Views
 {
-  /// <summary>
-  /// Interaction logic for FloatEditorView.xaml
-  /// </summary>
-  public partial class FloatEditorView : UserControl
+  public partial class FloatEditorView : IViewFor<FloatEditorViewModel>
   {
+    #region ViewModel
+    public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel),
+        typeof(FloatEditorViewModel), typeof(FloatEditorView), new PropertyMetadata(null));
+
+    public FloatEditorViewModel ViewModel
+    {
+      get => (FloatEditorViewModel)GetValue(ViewModelProperty);
+      set => SetValue(ViewModelProperty, value);
+    }
+
+    object IViewFor.ViewModel
+    {
+      get => ViewModel;
+      set => ViewModel = (FloatEditorViewModel)value;
+    }
+    #endregion
+
     public FloatEditorView()
     {
       InitializeComponent();
+
+      this.WhenActivated(d => {
+        this.Bind(ViewModel, vm => vm.Value, v => v.UpDown.Value).DisposeWith(d);
+      });
     }
   }
 }
